9eba98d6af82a154a0bdcf3da164e9ae38fb3730
Matheus Iazzetta Rodrigues <1900419@SALAS.AULAS>
2019-09-26 19:47:00 -0300

    remove test class

--- com/grupoprimeiro/converte_hora.py
+++ com/grupoprimeiro/converte_hora.py
@@ -10,13 +10,3 @@ def converteHora(hora24, minuto24):
     if (hora24 > 12):
         hora24 -= 12
     return '%02d:%02d PM' % (hora24, minuto24)
-
-# Entrada de dados
-continuar = 'S'
-while (continuar == 'S'):
-    hora = int(raw_input('Informe o valor da hora: '))
-    minuto = int(raw_input('Informe o valor dos minutos: '))
-
-    print converteHora(hora, minuto)
-
-    continuar = raw_input('Deseja continuar (S/N): ').upper()
